6503621981047399f6caec71260212f47b97868f
Viktor <viktor@v.tsvil>
2020-04-21 17:02:23 +0300

    generator added

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from web_shop.bot.setup import app, set_webhook
+from web_shop.db.seeder import generate
 
 if __name__ == '__main__':
     set_webhook()
+    generate(category=3, product=10, news=4)
     app.run(port=8000, debug=True)
