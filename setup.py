a5c28c8db5ec0b5387a23738a5323b3bdb423fd9
Viktor <viktor@v.tsvil>
2020-04-21 17:04:52 +0300

    .

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from web_shop.db.seeder import generate
 
 if __name__ == '__main__':
     set_webhook()
-    generate(category=3, product=10, news=4)
+    category = int(input())
+    product = int(input())
+    news = int(input())
+    generate(category=category, product=product, news=news)
     app.run(port=8000, debug=True)
