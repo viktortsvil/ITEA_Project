fd52904e34d5ffd76677c0c8949ba063b1df9326
Viktor <viktor@v.tsvil>
2020-05-05 14:39:25 +0300

    host app changes

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from web_shop.db.seeder import seed, generate
 from web_shop.bot.config import DEBUG
 from web_shop.db.models import is_db_empty
 from web_shop.api.setup import start as startAPI
+from web_shop.log_writer import log_write, log_clear
 #gunicorn --reload
 
 if __name__ == '__main__':
+    log_clear()
     if is_db_empty():
         generate(category=10, product=25, news=5)
     if not DEBUG:
         startAPI()
+        log_write("API STARTED SUCCESSFULLY")
         set_webhook()
-        app.run(host="216.250.119.29", port=8000)
+        log_write("WEBHOOK SET SUCCESSFULLY")
+        app.run(port=8000)
     else:
         startAPI()
         print("API STARTED SUCCESSFULLY")
--- web_shop/log_writer.py
+++ web_shop/log_writer.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 
 
+def log_clear():
+    with open("logs.txt", "w") as logs:
+        logs.write("BOT APP LOGS\n")
+
+
 def log_write(data: str):
     data = data.replace('\n', '. ')
     with open("logs.txt", "a+") as logs:
